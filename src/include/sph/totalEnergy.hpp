2f92f555f00ab37c731a16301bfcb622ce2d0207
Sebastian Keller <keller@cscs.ch>
2019-08-21 18:27:51 +0200

    energy calc -> hpx

--- src/include/sph/totalEnergy.hpp
+++ src/include/sph/totalEnergy.hpp
@@ -2,6 +2,7 @@
 
 #include <vector>
 #include <iostream>
+#include <tuple>
 
 #ifdef USE_MPI
 #include "mpi.h"
@@ -27,15 +28,48 @@ void computeTotalEnergy(const std::vector<int> &l, Dataset &d)
     T &eint = d.eint;
 
     T ecintmp = 0.0, einttmp = 0.0;
-#pragma omp parallel for reduction(+ : ecintmp, einttmp)
-    for (int pi = 0; pi < n; pi++)
+    //#pragma omp parallel for reduction(+ : ecintmp, einttmp)
+    //for (int pi = 0; pi < n; pi++)
+    //{
+    //    int i = clist[pi];
+
+    //    T vmod2 = vx[i] * vx[i] + vy[i] * vy[i] + vz[i] * vz[i];
+    //    ecintmp += 0.5 * m[i] * vmod2;
+    //    einttmp += u[i] * m[i];
+    //}
+
+    auto policy = hpx::parallel::execution::par;
+
+    //auto sum_op1 =
+    //    [&d](T tmp, int i)
+    //    {
+    //        T vmod2 = vx[i] * vx[i] + vy[i] * vy[i] + vz[i] * vz[i];
+    //        tmp += 0.5 * d.m.at(i) * vmod2;
+    //        return tmp;
+    //    };
+    //auto sum_op2 =
+    //    [=](T tmp, int i)
+    //    {
+    //        tmp += u[i] * m[i];
+    //        return tmp;
+    //    };
+
+    //ecintmp = hpx::parallel::reduce(policy, clist, clist+n, ecintmp, sum_op1);
+    //einttmp = hpx::parallel::reduce(policy, clist, clist+n, einttmp, sum_op2);
+
+    std::vector<double> ecinV(n), eintV(n);
+    hpx::parallel::for_loop(policy,
+        0, n,
+        [&ecinV, &eintV, clist, vx, vy, vz, m, u](int pi)
     {
         int i = clist[pi];
-
         T vmod2 = vx[i] * vx[i] + vy[i] * vy[i] + vz[i] * vz[i];
-        ecintmp += 0.5 * m[i] * vmod2;
-        einttmp += u[i] * m[i];
-    }
+        ecinV[i] = 0.5 * m[i] * vmod2;
+        eintV[i] = u[i] * m[i]; 
+    });
+
+    ecintmp = hpx::parallel::reduce(policy, std::begin(ecinV), std::end(ecinV), ecintmp);
+    einttmp = hpx::parallel::reduce(policy, std::begin(eintV), std::end(eintV), einttmp);
 
 #ifdef USE_MPI
     MPI_Allreduce(MPI_IN_PLACE, &ecintmp, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
