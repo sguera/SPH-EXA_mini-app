34e144392ea2771e23ad0fbf19ddd4db48c6be5b
Sebastian Keller <keller@cscs.ch>
2019-08-21 15:19:17 +0200

    hpx foor_loop for density

--- src/include/sph/density.hpp
+++ src/include/sph/density.hpp
@@ -31,6 +31,9 @@ void computeDensity(const std::vector<int> &l, Dataset &d)
     const T K = d.K;
 
 #if defined(USE_OMP_TARGET)
+
+    // OpenMP Target offload
+
     const size_t np = d.x.size();
     const size_t allNeighbors = n * ngmax;
 
@@ -45,64 +48,117 @@ void computeDensity(const std::vector<int> &l, Dataset &d)
 
         const int *neighbors = d.neighbors.data() + neighborsOffset;
 
-// clang-format off
-#pragma omp target map(to                                               \
+    // clang-format off
+    #pragma omp target map(to                                               \
 		       : clist [:n], neighbors [:neighborsChunkSize], neighborsCount [:n],  \
                          m [:np], h [:np], x [:np], y [:np], z [:np])                                                                      \
                    map(from                                                                                                                \
                        : ro [:n])
-// clang-format on
-#pragma omp teams distribute parallel for // dist_schedule(guided)
+    // clang-format on
+    #pragma omp teams distribute parallel for // dist_schedule(guided)
+
 #elif defined(USE_ACC)
+
+    // OpenACC
+
     const size_t neighborsOffset = 0;
     const int *neighbors = d.neighbors.data();
     const size_t np = d.x.size();
     const size_t allNeighbors = n * ngmax;
     const size_t begin_n = 0;
     const size_t end_n = n;
-#pragma acc parallel loop copyin(n, clist [0:n], neighbors [0:allNeighbors], neighborsCount [0:n], m [0:np], h [0:np], x [0:np], y [0:np], \
-                                 z [0:np]) copyout(ro [0:n])
+    #pragma acc parallel loop copyin(n, clist [0:n], neighbors [0:allNeighbors], neighborsCount [0:n], \
+                                     m [0:np], h [0:np], x [0:np], y [0:np], z [0:np]) copyout(ro [0:n])
+
 #else
+
+    // OpenMP / HPX
+
     const size_t neighborsOffset = 0;
     const int *neighbors = d.neighbors.data();
     const size_t begin_n = 0;
     const size_t end_n = n;
-#pragma omp parallel for
+    //#pragma omp parallel for
+
 #endif
-        for (size_t pi = begin_n; pi < end_n; ++pi)
-        {
-            const int i = clist[pi];
-            const int nn = neighborsCount[pi];
+        //for (size_t pi = begin_n; pi < end_n; ++pi)
+        //{
+        //    const int i = clist[pi];
+        //    const int nn = neighborsCount[pi];
 
-            T roloc = 0.0;
-            ro[i] = 0.0;
+        //    T roloc = 0.0;
+        //    ro[i] = 0.0;
 
-            // int converstion to avoid a bug that prevents vectorization with some compilers
-            for (int pj = 0; pj < nn; pj++)
+        //    // int converstion to avoid a bug that prevents vectorization with some compilers
+        //    for (int pj = 0; pj < nn; pj++)
+        //    {
+        //        const int j = neighbors[pi * ngmax + pj - neighborsOffset];
+        //        // later can be stores into an array per particle
+        //        // store the distance from each neighbor
+        //        const T dist = distancePBC(bbox, h[i], x[i], y[i], z[i], x[j], y[j], z[j]);
+
+        //        // calculate the v as ratio between the distance and the smoothing length
+        //        const T vloc = dist / h[i];
+
+        //        #ifndef NDEBUG
+        //        if (vloc > 2.0 + 1e-6 || vloc < 0.0)
+        //            printf("ERROR:Density(%d,%d) vloc %f -- x %f %f %f -- %f %f %f -- dist %f -- hi %f\n",
+        //                   i, j, vloc, x[i], y[i], z[i], x[j], y[j], z[j], dist, h[i]);
+        //        #endif
+
+        //        const T value = wharmonic(vloc, h[i], sincIndex, K);
+        //        roloc += value * m[j];
+        //    }
+
+        //    ro[i] = roloc + m[i] * K / (h[i] * h[i] * h[i]);
+
+        //    #ifndef NDEBUG
+        //    if (std::isnan(ro[i])) printf("ERROR::Density(%d) density %f, position: (%f %f %f), h: %f\n",
+        //                                  i, ro[i], x[i], y[i], z[i], h[i]);
+        //    #endif
+        //}
+
+        auto policy = hpx::parallel::execution::par; 
+        hpx::parallel::for_loop(policy,
+            begin_n, end_n,
+            [=](int pi)
             {
-                const int j = neighbors[pi * ngmax + pj - neighborsOffset];
-                // later can be stores into an array per particle
-                const T dist = distancePBC(bbox, h[i], x[i], y[i], z[i], x[j], y[j], z[j]); // store the distance from each neighbor
+                const int i = clist[pi];
+                const int nn = neighborsCount[pi];
 
-                // calculate the v as ratio between the distance and the smoothing length
-                const T vloc = dist / h[i];
+                T roloc = 0.0;
+                ro[i] = 0.0;
 
-#ifndef NDEBUG
-                if (vloc > 2.0 + 1e-6 || vloc < 0.0)
-                    printf("ERROR:Density(%d,%d) vloc %f -- x %f %f %f -- %f %f %f -- dist %f -- hi %f\n", i, j, vloc, x[i], y[i], z[i],
-                           x[j], y[j], z[j], dist, h[i]);
-#endif
+                // int converstion to avoid a bug that prevents vectorization with some compilers
+                for (int pj = 0; pj < nn; pj++)
+                {
+                    const int j = neighbors[pi * ngmax + pj - neighborsOffset];
+                    // later can be stores into an array per particle
+                    // store the distance from each neighbor
+                    const T dist = distancePBC(bbox, h[i], x[i], y[i], z[i], x[j], y[j], z[j]);
 
-                const T value = wharmonic(vloc, h[i], sincIndex, K);
-                roloc += value * m[j];
+                    // calculate the v as ratio between the distance and the smoothing length
+                    const T vloc = dist / h[i];
+
+                    //#ifndef NDEBUG
+                    //if (vloc > 2.0 + 1e-6 || vloc < 0.0)
+                    //    printf("ERROR:Density(%d,%d) vloc %f -- x %f %f %f -- %f %f %f -- dist %f -- hi %f\n",
+                    //           i, j, vloc, x[i], y[i], z[i], x[j], y[j], z[j], dist, h[i]);
+                    //#endif
+
+                    const T value = wharmonic(vloc, h[i], sincIndex, K);
+                    roloc += value * m[j];
+                }
+
+                ro[i] = roloc + m[i] * K / (h[i] * h[i] * h[i]);
+
+                //#ifndef NDEBUG
+                //if (std::isnan(ro[i])) printf("ERROR::Density(%d) density %f, position: (%f %f %f), h: %f\n",
+                //                              i, ro[i], x[i], y[i], z[i], h[i]);
+                //#endif
             }
+        );
 
-            ro[i] = roloc + m[i] * K / (h[i] * h[i] * h[i]);
-
-#ifndef NDEBUG
-            if (std::isnan(ro[i])) printf("ERROR::Density(%d) density %f, position: (%f %f %f), h: %f\n", i, ro[i], x[i], y[i], z[i], h[i]);
-#endif
-        }
 #if defined(USE_OMP_TARGET)
     }
 #endif
